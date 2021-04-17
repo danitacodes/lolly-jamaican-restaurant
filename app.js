4bea111a50d83947e408bd8b2adc182d69232837
Danita Forbes <danitacodes@gmail.com>
2021-04-16 22:33:51 -0700

    added sass files

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const hamburgerMenu = document.querySelector('#navigation .nav-icon');
 const navContent = document.querySelector('#nav-content');
+const closeNavButton = document.querySelector('#nav-content .close-btn');
 
 hamburgerMenu.addEventListener('click', ()=> {
     navContent.classList.add('show');
-});
+})
+
+closeNavButton.addEventListener('click', ()=> {
+    navContent.classList.remove('show');
+})
+
